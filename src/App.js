b8fa2528ae66af00ed963681283c6b85492244cf
bhawnas <bhawnas@thoughtworks.com>
2020-08-28 23:41:04 +0530

    Redux with hooks, Replace connect with useSelector, useDispatch

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
 import React from 'react';
 import './App.css';
 import {addAction} from './actions';
-import {connect} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import List from "./List";
 
-class App extends React.Component {
-    render() {
-        let input;
-        return (
-            <div>
-                <label>My List</label>
-                <List items={this.props.items}/>
-                <form onSubmit={e => {
-                    e.preventDefault();
-                    this.props.addItem(input.value);
-                    input.value = ''
-                }}>
-                    <input ref={e => (input=e)}/>
-                    <button type="submit">
-                        Add me!
-                    </button>
-                </form>
-            </div>
-        );
-    };
-}
-const mapStateToProps = (state) => {
-    return {
-        items: state
-    }
+export const App = () => {
+    const items = useSelector(state => state);
+    const dispatch = useDispatch();
+    let input;
+    return (
+        <div>
+            <label>My List</label>
+            <List items={items}/>
+            <form onSubmit={e => {
+                e.preventDefault();
+                dispatch(addAction(input.value));
+                input.value = ''
+            }}>
+                <input ref={e => (input = e)}/>
+                <button type="submit">
+                    Add me!
+                </button>
+            </form>
+        </div>
+    );
 };
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        addItem: text => dispatch(addAction(text))
-    }
-};
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+// const mapStateToProps = (state) => {
+//     return {
+//         items: state
+//     }
+// };
+//
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//         addItem: text => dispatch(addAction(text))
+//     }
+// };
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import './index.css';
-import App from './App';
+import {App} from './App';
 import {reducer} from './reducers';
 import {createStore} from 'redux';
 
